0406af2e528957820871b6ead2bcb2267327231f
Dam450 <68165752+dam450@users.noreply.github.com>
2023-09-20 14:42:34 -0300

    chore/TASK-0001/SUB-005: set default theme and global style

---
+++ src/presentation/styles/themes/default.js
@@ -0,0 +1,29 @@
+export const defaultTheme = {
+  COLORS: {
+    white: '#FDFDFD',
+
+    'zinc-50': '#f3f4f6',
+    'zinc-100': '#cecfd6',
+    'zinc-150': '#b5b7c1',
+    'zinc-200': '#888ca6',
+    'zinc-250': '#848898',
+    'zinc-400': '#53586e',
+    'zinc-500': '#3a4059',
+
+    'blue-titan': '#dee3ff',
+    'blue-sky': '#5490f9',
+    'blue-sky-50': '#3e66a7',
+    'blue-sky-100': '#4a66bb',
+    'blue-sky-150': '#5968d3',
+    'blue-sky-500': '#021e4c',
+    'blue-sky-600': '#10184d',
+    'blue-sky-700': '#091030',
+
+    tomate: '#ff5353',
+  },
+
+  GRADIENTS: {
+    background:
+      'linear-gradient(270deg, #031E4C 0.05%, #062E60 30.9%, #1164A2 94.23%)',
+  },
+}
