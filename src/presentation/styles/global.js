e1a4a3874ad22d19debe885d86a45c19343b5e64
Dam450 <68165752+dam450@users.noreply.github.com>
2023-09-21 17:13:12 -0300

    chore/TASK-0001/SUB-005: set default font-size on global style

--- src/presentation/styles/global.js
+++ src/presentation/styles/global.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+
+  :root {
+    font-size: 62.5%;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -11,5 +16,6 @@ export const GlobalStyle = createGlobalStyle`
     background: ${props => props.theme.GRADIENTS.background};
     color: ${({ theme }) => theme.COLORS.white};
     font-family: ${props => props.theme.FONTS.Primary};
+    font-size: 1.6rem
   }
 `
