a5ad64d326748d8b8717e1b305c299ed7f6d667b
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-09-25 18:30:50 -0300

    feat/TASK-0003: Created component to show video

--- src/presentation/components/Video/index.jsx
+++ src/presentation/components/Video/index.jsx
@@ -1,4 +1,13 @@
-import { Container } from './styles'
+import { Container, StyledVideo, FallbackText } from './styles'
+import video from '../../assets/video-sample.mp4'
+
 export function Video() {
-  return <Container></Container>
+  return (
+    <Container>
+      <StyledVideo controls autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+        <button type="button">+</button>
+      </StyledVideo>
+    </Container>
+  )
 }
--- src/presentation/components/Video/styles.js
+++ src/presentation/components/Video/styles.js
@@ -1,3 +1,19 @@
-import { styled } from 'styled-components'
+import styled from 'styled-components'
 
-export const Container = styled.video``
+export const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  text-align: center;
+  position: relative;
+`
+
+export const StyledVideo = styled.video`
+  width: 100%;
+  max-height: 400px;
+  background-color: #000;
+`
+
+export const FallbackText = styled.p`
+  font-size: 16px;
+  color: #ff0000;
+`
