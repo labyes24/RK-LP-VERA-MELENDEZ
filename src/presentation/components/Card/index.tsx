1c10ac6cb534fc0a9b1fe05c3d436b30e3f2baa2
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2023-09-25 22:22:36 -0300

    chore/TASK-0006: set Card content

--- src/presentation/components/Card/index.tsx
+++ src/presentation/components/Card/index.tsx
@@ -1,7 +1,15 @@
+import React from 'react'
+import { CardContainer } from './styles'
+
 export function Card() {
   return (
-    <>
-      <h1>Card</h1>
-    </>
+    <CardContainer>
+      <blockquote>
+        "Winnetou apresentou-se bastante solícito e acolhedor. Acompanhou cada
+        etapa do processo com bastante cuidado. Profissional excelente,
+        capacitado e atencioso com cada detalhe."
+      </blockquote>
+      <cite>Paola Oliveira, Atriz</cite>
+    </CardContainer>
   )
 }
