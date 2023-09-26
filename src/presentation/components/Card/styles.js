c63a95d95c39c3d8a3a01650b3040b802e5ccd6e
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2023-09-25 22:20:32 -0300

    style/TASK-0006: update component Card styling for desktop

--- src/presentation/components/Card/styles.js
+++ src/presentation/components/Card/styles.js
@@ -1,3 +1,34 @@
 import styled from 'styled-components'
 
-export const CardContainer = styled.div``
+export const CardContainer = styled.div`
+  background-color: ${({ theme }) => theme.COLORS['zinc-500']};
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+
+  border-radius: 8px;
+  padding: 3.2rem 1.6rem;
+  gap: 2.4rem;
+
+  max-width: 28rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > blockquote {
+    font-family: ${({ theme }) => theme.FONTS.Secondary};
+    color: ${({ theme }) => theme.COLORS.white};
+    font-style: italic;
+    font-size: 1.2rem;
+    text-align: center;
+    line-height: 1.4;
+  }
+
+  > cite {
+    font-family: ${({ theme }) => theme.FONTS.Primary};
+    color: ${({ theme }) => theme.COLORS['blue-sky']};
+    font-weight: bold;
+    font-style: normal;
+    font-size: 1.6rem;
+    line-height: 1;
+  }
+`
