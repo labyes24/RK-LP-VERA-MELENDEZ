a541d14ca92abb5035f9de8f26701363cb6bbaf9
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2023-09-27 15:59:15 -0300

    style/TASK-0006: add media breakpoints

--- src/presentation/components/Card/styles.js
+++ src/presentation/components/Card/styles.js
@@ -1,18 +1,22 @@
 import styled from 'styled-components'
 
 export const CardContainer = styled.div`
+  border: 1px solid green; // TODO: remove this border
+
   background-color: ${({ theme }) => theme.COLORS['zinc-500']};
   box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
 
-  border-radius: 8px;
+  border-radius: 6px;
   padding: 3.2rem 1.6rem;
   gap: 2.4rem;
 
-  max-width: 28rem;
+  width: 17.5rem;
+  height: 14rem;
 
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 
   > blockquote {
     font-family: ${({ theme }) => theme.FONTS.Secondary};
@@ -30,5 +34,13 @@ export const CardContainer = styled.div`
     font-style: normal;
     font-size: 1.6rem;
     line-height: 1;
+  }
+
+  @media (min-width: 768px) {
+    border: 1px solid red; // TODO: remove this border
+
+    border-radius: 8px;
+    width: 28rem;
+    height: 19rem;
   }
 `
