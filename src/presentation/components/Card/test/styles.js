e80390c32c6be4eae089f6d1bc0a82273d215275
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2023-10-02 11:59:05 -0300

    chore/TASK-0006: add styled scroll for test

--- src/presentation/components/Card/test/styles.js
+++ src/presentation/components/Card/test/styles.js
@@ -7,16 +7,60 @@ export const CardTestContainer = styled.div`
   width: 100vw;
   padding: 1.5rem;
   gap: 1rem;
-  overflow: hidden;
 
   .row-group {
     min-width: 42rem;
+    min-height: 20rem;
 
     display: flex;
     gap: 1rem;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
 
     overflow-x: auto;
+    overflow-y: hidden;
+
+    padding-bottom: 1rem;
+
+    /* Scroll Bar */
+
+    /* Firefox */
+    & {
+      scrollbar-width: thin;
+      scrollbar-color: ${({ theme }) => theme.COLORS['blue-sky']}
+        ${({ theme }) => theme.COLORS['zinc-100']};
+    }
+
+    /* Chrome, Edge and Safari */
+    &::-webkit-scrollbar {
+      height: 6px;
+      width: 6px;
+    }
+
+    &::-webkit-scrollbar-track {
+      border-radius: 8px;
+      background-color: ${({ theme }) => theme.COLORS['zinc-100']};
+    }
+
+    &::-webkit-scrollbar-track:hover {
+      background-color: ${({ theme }) => theme.COLORS['zinc-150']};
+    }
+
+    &::-webkit-scrollbar-track:active {
+      background-color: ${({ theme }) => theme.COLORS['zinc-150']};
+    }
+
+    &::-webkit-scrollbar-thumb {
+      border-radius: 5px;
+      background-color: ${({ theme }) => theme.COLORS['blue-sky']};
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+      background-color: ${({ theme }) => theme.COLORS['blue-sky-50']};
+    }
+
+    &::-webkit-scrollbar-thumb:active {
+      background-color: ${({ theme }) => theme.COLORS['blue-sky-50']};
+    }
   }
 `
