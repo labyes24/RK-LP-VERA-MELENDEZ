037c5c410188ceb79c239761d834aa6dbc2e8184
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-10-02 10:49:14 -0300

    feat/TASK-0007: added component Spinner routes

--- src/router/app.routes.jsx
+++ src/router/app.routes.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { SpinnerTest } from '../presentation/components/Spinner/test'
 
 export function RoutesApp() {
   return (
@@ -13,7 +14,7 @@ export function RoutesApp() {
         path="/RealEstateBrokerDescription"
         element={<h2>RealEstateBrokerDescription</h2>}
       />
-      <Route path="/Spinner" element={<h2>Spinner</h2>} />
+      <Route path="/Spinner" element={<SpinnerTest />} />
       <Route path="/Card" element={<h2>Card</h2>} />
       <Route path="/WhatsappButton" element={<h2>WhatsappButton</h2>} />
       <Route path="*" element={<h2>Rota inexistente</h2>} />
