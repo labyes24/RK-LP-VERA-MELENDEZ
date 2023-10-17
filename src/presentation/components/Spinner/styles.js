cd651d2c792a39bb2aa3f386dc6c80fb590296f0
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-10-17 12:58:48 -0300

    style/feat-0002:implementation of QA suggestions

--- src/presentation/components/Spinner/styles.js
+++ src/presentation/components/Spinner/styles.js
@@ -12,10 +12,10 @@ const spin = keyframes`
 `
 
 export const Loader = styled.div`
-  border: 8px solid ${({ theme }) => theme.COLORS['blue-sky']};
+  border: 6px solid ${({ theme }) => theme.COLORS['blue-sky']};
   border-top-color: ${({ theme }) => theme.COLORS['blue-sky-500']};
   border-radius: 50%;
-  width: 10rem;
+  width: 8rem;
   animation: ${spin} 1s linear infinite;
   aspect-ratio: 1/1;
 `
--- src/presentation/components/Spinner/test/styles.js
+++ src/presentation/components/Spinner/test/styles.js
@@ -2,15 +2,16 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
-  margin: 20rem auto 20rem;
+  margin: auto;
   align-items: center;
   justify-content: center;
 `
 
 export const Button = styled.button`
   display: flex;
+  margin: -40rem auto -20rem;
   width: 21rem;
   height: 3rem;
   padding: 8px 88px;
@@ -21,7 +22,7 @@ export const Button = styled.button`
   color: ${({ theme }) => theme.COLORS.white};
   font-family: ${({ theme }) => theme.FONTS.Primary};
   font-size: 1.2rem;
-  font-weight: bold;
+  font-weight: 700;
   border: none;
   border-radius: 0.4rem;
   background: ${({ theme }) => theme.COLORS['blue-sky-600']};
