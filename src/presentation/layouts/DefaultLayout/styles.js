c0803a5f23147ea7269970aba811aa591dbd2086
Dam450 <68165752+dam450@users.noreply.github.com>
2023-10-18 18:03:50 -0300

    style/TASK-0018: styling Default Layout

---
+++ src/presentation/layouts/DefaultLayout/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  width: 100%;
+  max-width: 144rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  .content {
+    flex: 1;
+  }
+`
