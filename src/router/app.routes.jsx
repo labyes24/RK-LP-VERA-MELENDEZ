1bce83bdefa2b0ccdefb42012e84a8d2fd08c0d9
Dam450 <68165752+dam450@users.noreply.github.com>
2023-10-18 18:21:28 -0300

    chore/TASK-0018: apply DefaultLayout to app routes

--- src/router/app.routes.jsx
+++ src/router/app.routes.jsx
@@ -1,17 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from '../presentation/layouts/DefaultLayout'
+
 import { SpinnerTest } from '../presentation/components/Spinner/test'
 import { BrokerImageTest } from '../presentation/components/BrokerImage/test'
 import { ModalTest } from '../presentation/pages/Properties/components/Modal/test'
 import { VideoTest } from '../presentation/components/Video/test'
 import { WhatsappButtonTest } from '../presentation/components/WhatsappButton/test/index'
 import { CardTest } from '../presentation/components/Card/test'
-
 import { RealEstateBrokerDescriptionTest } from '../presentation/pages/Profile/components/RealEstateBrokerDescription/test'
 
 export function RoutesApp() {
   return (
     <Routes>
-      <Route path="/" element={<h1>RK Landing Page</h1>} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<h1>RK Landing Page</h1>} />
+        <Route path="*" element={<h2>Rota inexistente</h2>} />
+      </Route>
+
+      {/* Test Routes */}
       <Route path="/Video" element={<VideoTest />} />
       <Route path="/RealEstateBrokerImage" element={<BrokerImageTest />} />
       <Route
@@ -22,7 +28,6 @@ export function RoutesApp() {
       <Route path="/Card" element={<CardTest />} />
       <Route path="/Modal" element={<ModalTest />} />
       <Route path="/WhatsappButton" element={<WhatsappButtonTest />} />
-      <Route path="*" element={<h2>Rota inexistente</h2>} />
     </Routes>
   )
 }
