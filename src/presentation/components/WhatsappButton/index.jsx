0f9d8e38f781f5bf2960c6747893bf982cdd2a0d
Mateus Villa Nova <villa1999nova@gmail.com>
2023-10-18 10:34:53 -0300

    refactor/FIX-0003/TASK-0008: Renamed variables

--- src/presentation/components/WhatsappButton/index.jsx
+++ src/presentation/components/WhatsappButton/index.jsx
@@ -1,7 +1,7 @@
 import { Container } from './style.js'
 import whatsappButton from '../../assets/whatsappButton.svg'
 
-const whatsappUrl = 'https://wa.me'
+const WHATSAPP_URL = 'https://wa.me'
 
 /**
  * Renders a Whatsapp button at the right bottom of the page.
@@ -14,15 +14,15 @@ const whatsappUrl = 'https://wa.me'
 export function WhatsappButton({ phoneNumber, message = null }) {
   phoneNumber = phoneNumber?.replace(/[^\w\s]/gi, '').replace(/ /g, '')
 
-  let Url = `${whatsappUrl}/55${phoneNumber}`
+  let url = `${WHATSAPP_URL}/55${phoneNumber}`
 
   if (message) {
-    Url += `/?text=${encodeURI(message)}`
+    url += `/?text=${encodeURI(message)}`
   }
 
   return (
     <Container>
-      <a href={Url} target="_blank" rel="noopener noreferrer">
+      <a href={url} target="_blank" rel="noopener noreferrer">
         <img src={whatsappButton} alt="Botão com icone do WhatsApp." />
       </a>
     </Container>
