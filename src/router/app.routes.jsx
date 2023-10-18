ce4876f2b0554ef269367acd3437de56df5d0fe1
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-10-18 20:25:04 -0300

    feat/TASK-0016: Added header test route

--- src/router/app.routes.jsx
+++ src/router/app.routes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from '../presentation/layouts/DefaultLayout'
 
+import { HeaderTest } from '../presentation/components/Header/test'
 import { SpinnerTest } from '../presentation/components/Spinner/test'
 import { BrokerImageTest } from '../presentation/components/BrokerImage/test'
 import { ModalTest } from '../presentation/pages/Properties/components/Modal/test'
@@ -19,6 +20,8 @@ export function RoutesApp() {
 
       {/* Test Routes */}
       <Route path="/Header" element={<div>Header</div>} />
+      <Route path="/" element={<h1>RK Landing Page</h1>} />
+      <Route path="/Header" element={<HeaderTest />} />
       <Route path="/Video" element={<VideoTest />} />
       <Route path="/RealEstateBrokerImage" element={<BrokerImageTest />} />
       <Route
