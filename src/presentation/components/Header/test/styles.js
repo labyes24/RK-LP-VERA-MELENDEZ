3ed5e9775a9477d0f7235f23c818533c22b013d9
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-10-19 11:57:18 -0300

    style/TASK-0016: Added padding to mobile devices

--- src/presentation/components/Header/test/styles.js
+++ src/presentation/components/Header/test/styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  padding: 0 4.8rem;
+  padding: 0 2rem;
+
+  @media (min-width: 1024px) {
+    padding: 0 4.8rem;
+  }
 `
