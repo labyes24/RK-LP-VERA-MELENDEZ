13dc1631e2ea97d695acd499ae3d67db0cb5d8e4
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-10-18 22:15:26 -0300

    style/TASK-0016: Added menu hamburger animation

--- src/presentation/components/Header/styles.js
+++ src/presentation/components/Header/styles.js
@@ -14,7 +14,64 @@ export const Logo = styled.img`
   width: 8rem;
 `
 
-export const ToggleHamburger = styled.button``
+export const ToggleHamburger = styled.div`
+  input {
+    display: none;
+  }
+
+  label {
+    position: relative;
+    width: 36px;
+    height: 36px;
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center;
+    gap: 6px;
+    transition-duration: 0.5s;
+  }
+
+  span {
+    width: 100%;
+    height: 5px;
+    background-color: ${({ theme }) => theme.COLORS.white};
+    border-radius: 4px;
+  }
+
+  span:nth-child(2) {
+    transition-duration: 0.6s;
+    width: 75%;
+  }
+
+  input:checked + label {
+    transition-duration: 0.5s;
+    transform: rotate(180deg);
+  }
+
+  input:checked + label span {
+    position: absolute;
+    transition-duration: 0.5s;
+    background-color: ${({ theme }) => theme.COLORS['blue-sky-700']};
+  }
+
+  input:checked + label span:first-child {
+    width: 100%;
+    transform: rotate(45deg);
+    transition-duration: 0.5s;
+  }
+
+  input:checked + label span:nth-child(2) {
+    transform: scaleX(0);
+    transition-duration: 0.1s;
+  }
+
+  input:checked + label span:last-child {
+    width: 100%;
+    transform: rotate(-45deg);
+    transition-duration: 0.5s;
+  }
+`
 
 export const Links = styled.ul`
   display: flex;
