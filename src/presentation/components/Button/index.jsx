774619e3492242dc616de48ba1ccc2dc0a05dde6
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-23 14:01:01 -0300

    feat/TASK-0013: added all the navite props to Button

--- src/presentation/components/Button/index.jsx
+++ src/presentation/components/Button/index.jsx
@@ -2,9 +2,14 @@ import { Spinner } from '../Spinner'
 
 import { StyledButton } from './styles'
 
-export function Button({ children, size = 'small', isLoading = false }) {
+export function Button({
+  children,
+  size = 'small',
+  isLoading = false,
+  ...props
+}) {
   return (
-    <StyledButton $size={size}>
+    <StyledButton $size={size} {...props}>
       {isLoading ? <Spinner size="small" /> : children}
     </StyledButton>
   )
