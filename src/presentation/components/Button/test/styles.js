6092178d30987b11b569f49a0d591bd976d54bbe
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-23 13:55:51 -0300

    style/TASK-0013: improved Button test styles

--- src/presentation/components/Button/test/styles.js
+++ src/presentation/components/Button/test/styles.js
@@ -2,4 +2,11 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   padding: 2rem;
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
 `
