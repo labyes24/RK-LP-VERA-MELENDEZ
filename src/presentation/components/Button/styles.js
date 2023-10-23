9d393ab9193bedfce69bd515a4370436d844030e
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-23 14:33:01 -0300

    style/TASK-0013: changed responsiveness styles

--- src/presentation/components/Button/styles.js
+++ src/presentation/components/Button/styles.js
@@ -5,13 +5,13 @@ export const StyledButton = styled.button`
   align-items: center;
   justify-content: center;
 
-  padding: ${({ $size }) => ($size === 'large' ? '2.4rem' : '1.6rem 2.4rem')};
-  gap: 1rem;
+  padding: ${({ $size }) =>
+    $size === 'large' ? '1.6rem 2.4rem' : '0.8rem 2.4rem'};
 
   border: none;
   color: ${({ theme }) => theme.COLORS.white};
   font-family: ${({ theme }) => theme.FONTS.Primary};
-  font-size: ${({ $size }) => ($size === 'large' ? '3.6rem' : '1.8rem')};
+  font-size: ${({ $size }) => ($size === 'large' ? '2.4rem' : '1.6rem')};
   font-style: normal;
   font-weight: 700;
   border-radius: ${({ $size }) => ($size === 'large' ? '1rem' : '0.4rem')};
@@ -25,9 +25,7 @@ export const StyledButton = styled.button`
   }
 
   @media screen and (min-width: 1440px) {
-    padding: ${({ $size }) =>
-      $size === 'large' ? '1.6rem 2.4rem' : '0.8rem 2.4rem'};
-
-    font-size: ${({ $size }) => ($size === 'large' ? '2.4rem' : '1.6rem')};
+    padding: ${({ $size }) => ($size === 'large' ? '2.4rem' : '1.6rem 2.4rem')};
+    font-size: ${({ $size }) => ($size === 'large' ? '3.6rem' : '1.8rem')};
   }
 `
