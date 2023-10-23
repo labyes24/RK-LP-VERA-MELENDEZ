57710c405ffc548231b6608d57e33ba4c413e03f
Mateus Villa Nova <villa1999nova@gmail.com>
2023-10-23 19:24:24 -0300

    chore/TASK-0012/SUB-0002: Att link to google maps

--- src/presentation/components/ContactBlock/index.jsx
+++ src/presentation/components/ContactBlock/index.jsx
@@ -14,7 +14,7 @@ export function ContactBlock({ address, email, phoneNumber }) {
             <p>
               <a
                 href={
-                  'https://www.google.com.br/maps/place/R.+das+Gaivotas,+1709+-+Ingleses+Norte,+Florian%C3%B3polis+-+SC,+88058-500/@-27.4231078,-48.4046376,17z/data=!3m1!4b1!4m6!3m5!1s0x9527420b0fdb2cb9:0xc0cb19ae8e111a32!8m2!3d-27.4231126!4d-48.4020627!16s%2Fg%2F11c5j12tcd?entry=ttu'
+                  'https://www.google.com.br/maps/place/RK/@-27.4232696,-48.4016947,3a,75y,234.74h,91.99t/data=!3m6!1e1!3m4!1sb9QPUO91m8kBQzoYTIg-QA!2e0!7i16384!8i8192!4m16!1m9!3m8!1s0x9527420b0fdb2cb9:0xc0cb19ae8e111a32!2sR.+das+Gaivotas,+1709+-+Ingleses+Norte,+Florian%C3%B3polis+-+SC,+88058-500!3b1!8m2!3d-27.4231126!4d-48.4020627!10e5!16s%2Fg%2F11c5j12tcd!3m5!1s0x9527420b068878f5:0x4b86a614a636e7a9!8m2!3d-27.4233434!4d-48.4018518!16s%2Fg%2F1tdbxyhw?entry=ttu'
                 }
                 target="_blank"
                 rel="noopener noreferrer"
