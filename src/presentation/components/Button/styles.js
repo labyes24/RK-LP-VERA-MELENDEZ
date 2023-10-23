ab8c11adabac0b7ec79ac7c2de84ef9778d7b273
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-23 13:35:11 -0300

    style/TASK-0013: added Button basic styles

--- src/presentation/components/Button/styles.js
+++ src/presentation/components/Button/styles.js
@@ -1,5 +1,28 @@
 import styled from 'styled-components'
 
 export const StyledButton = styled.button`
-  background: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 2.4rem;
+  gap: 1rem;
+
+  border: none;
+  color: ${({ theme }) => theme.COLORS.white};
+  font-family: ${({ theme }) => theme.FONTS.Primary};
+  font-size: 3.6rem;
+  font-style: normal;
+  font-weight: 700;
+  border-radius: 1rem;
+  background-color: ${({ theme }) => theme.COLORS['blue-sky-600']};
+
+  cursor: pointer;
+  transition: 0.3s background-color;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.COLORS['blue-sky-700']};
+  }
 `
+
+/* passar border-radius e font-size por props  */
