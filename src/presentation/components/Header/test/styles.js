b8856ba2b09dd0d317de101dc57192a64f851afb
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-10-24 11:14:57 -0300

    style/TASK-0016: Added style to tests environment

--- src/presentation/components/Header/test/styles.js
+++ src/presentation/components/Header/test/styles.js
@@ -1,10 +1,19 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  padding: 0 2rem;
   width: 100%;
+  max-width: 144rem;
 
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  padding: 0 2rem;
   @media (min-width: 1024px) {
     padding: 0 4.8rem;
+  }
+
+  .content {
+    flex: 1;
   }
 `
