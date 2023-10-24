ace249f667f679a80db989ae4318f77e9820211f
adriano.souza <adriano.souza@zpesystems.com>
2023-10-24 19:20:26 -0300

    feat/TASK-0012/SUB-0001: Fixing rebase

--- src/presentation/layouts/DefaultLayout/index.jsx
+++ src/presentation/layouts/DefaultLayout/index.jsx
@@ -1,6 +1,9 @@
 import { Outlet } from 'react-router-dom'
 import { LayoutContainer } from './styles'
 import { Header } from '../../components/Header'
+
+import { Footer } from '../../components/Footer'
+
 export function DefaultLayout() {
   return (
     <LayoutContainer>
@@ -10,9 +13,7 @@ export function DefaultLayout() {
         <Outlet />
       </div>
 
-      <footer>
-        <p>Footer</p>
-      </footer>
+      <Footer />
     </LayoutContainer>
   )
 }
