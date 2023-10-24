994a3fd9722071e5128591fe984a4518da492c3a
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-10-24 11:16:46 -0300

    feat/TASK-0016: Added simulated default layout in test environment

--- src/presentation/components/Header/test/index.jsx
+++ src/presentation/components/Header/test/index.jsx
@@ -1,10 +1,18 @@
-import { Header } from '../'
+import { Outlet } from 'react-router-dom'
 import { Container } from './styles'
+import { Header } from '..'
 
 export function HeaderTest() {
   return (
     <Container>
       <Header />
+      <div className="content">
+        <Outlet />
+      </div>
+
+      <footer>
+        <p>Footer</p>
+      </footer>
     </Container>
   )
 }
