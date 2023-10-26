51f01e4f347aa7e7cab16f8c3331794744aee2d7
Caio Balduino <caiobrafachino@hotmail.com>
2023-10-26 18:35:04 -0300

    feat/<TASK-0023>: creating files and importing necessary components to page

---
+++ src/presentation/pages/Contact/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from './styles'
+import { Header } from '../../components/Header'
+import { BrokerImage } from '../../components/BrokerImage'
+import { Footer } from '../../components/Footer'
+
+export function Contact() {
+  
+  return (
+  
+  
+  
+  )
+  
+}
