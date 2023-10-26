518991ec4df18a4ba72ef5ad667489859223e591
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-26 14:35:39 -0300

    feat/TASK-0020: created quotesList to add dynamic Cards

--- src/presentation/pages/Profile/index.jsx
+++ src/presentation/pages/Profile/index.jsx
@@ -1,14 +1,36 @@
 import { Card } from '../../components/Card'
 import { Container } from './styles'
 
+const quotesList = [
+  {
+    quote:
+      '“Winnetou apresentou-se bastante solícito e acolhedor. Acompanhou cada etapa do processo com bastante cuidado. Profissional excelente, capacitado e atencioso com cada detalhe.”',
+    author: 'Paola Oliveira, Atriz',
+  },
+  {
+    quote:
+      '“Winnetou apresentou-se bastante solícito e acolhedor. Acompanhou cada etapa do processo com bastante cuidado. Profissional excelente, capacitado e atencioso com cada detalhe.”',
+    author: 'Paola Oliveira, Atriz',
+  },
+  {
+    quote:
+      '“Fazer negócios com a RK Imóveis é garantia de sucesso. Eu já comprei, vendi, fui locador e locatário de imóveis, e em todas as negociações fui bem atendido.”',
+    author: 'Manoel de Barros, Poeta',
+  },
+  {
+    quote:
+      '"Excelente atendimento, quando anunciei meu imóvel, foram muito atenciosos, e venderam ele muito rápido. Empresa séria e transparente. Recomendo!"',
+    author: 'Madam CJ Walker, Empresária',
+  },
+]
+
 export function Profile() {
   return (
     <Container>
       <section>
-        <Card quote={'teste'} author={'testador'} />
-        <Card quote={'teste'} author={'testador'} />
-        <Card quote={'teste'} author={'testador'} />
-        <Card quote={'teste'} author={'testador'} />
+        {quotesList.map((quote, index) => (
+          <Card key={index} {...quote} />
+        ))}
       </section>
     </Container>
   )
