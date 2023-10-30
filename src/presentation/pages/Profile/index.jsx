7bcb9403392a31292f81b7959770471d0b360d3e
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-30 14:48:45 -0300

    refactor/TASK-0020: using new approach to get keys

--- src/presentation/pages/Profile/index.jsx
+++ src/presentation/pages/Profile/index.jsx
@@ -7,21 +7,25 @@ const quotesList = [
     quote:
       '“Winnetou apresentou-se bastante solícito e acolhedor. Acompanhou cada etapa do processo com bastante cuidado. Profissional excelente, capacitado e atencioso com cada detalhe.”',
     author: 'Paola Oliveira, Atriz',
+    id: crypto.randomUUID(),
   },
   {
     quote:
       '“Winnetou apresentou-se bastante solícito e acolhedor. Acompanhou cada etapa do processo com bastante cuidado. Profissional excelente, capacitado e atencioso com cada detalhe.”',
     author: 'Paola Oliveira, Atriz',
+    id: crypto.randomUUID(),
   },
   {
     quote:
       '“Fazer negócios com a RK Imóveis é garantia de sucesso. Eu já comprei, vendi, fui locador e locatário de imóveis, e em todas as negociações fui bem atendido.”',
     author: 'Manoel de Barros, Poeta',
+    id: crypto.randomUUID(),
   },
   {
     quote:
       '"Excelente atendimento, quando anunciei meu imóvel, foram muito atenciosos, e venderam ele muito rápido. Empresa séria e transparente. Recomendo!"',
     author: 'Madam CJ Walker, Empresária',
+    id: crypto.randomUUID(),
   },
 ]
 
@@ -31,8 +35,8 @@ export function Profile() {
       <RealEstateBrokerDescription />
 
       <section>
-        {quotesList.map((quote, index) => (
-          <Card key={index} {...quote} />
+        {quotesList.map(quote => (
+          <Card key={quote.id} {...quote} />
         ))}
       </section>
     </Container>
