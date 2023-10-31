d0ca722837276b96e62e633409695e6cb87188fc
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2023-10-31 16:16:33 -0300

    chore/TASK-0024/SUB-0003: create ContactForm test

---
+++ src/presentation/pages/Contact/components/ContactForm/test/index.jsx
@@ -0,0 +1,11 @@
+import { ContactForm } from '../'
+
+export function ContactFormTest() {
+  return (
+    <>
+      <div style={{ width: '70rem', margin: '0 auto', padding: '2rem 1rem' }}>
+        <ContactForm />
+      </div>
+    </>
+  )
+}
