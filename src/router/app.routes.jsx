976cb596a2a9caad902c7ea2a56ca61865fe7d41
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2023-10-31 10:07:51 -0300

    chore/TASK-0024/SUB-0001: add SelectOption test route

--- src/router/app.routes.jsx
+++ src/router/app.routes.jsx
@@ -14,6 +14,7 @@ import { ButtonTest } from '../presentation/components/Button/test'
 import { InputTextTest } from '../presentation/components/TextInput/test'
 import { FooterTest } from '../presentation/components/Footer/test'
 import { ContactBlockTest } from '../presentation/components/ContactBlock/test'
+import { SelectOptionTest } from '../presentation/components/SelectOption/test'
 
 import { Profile } from '../presentation/pages/Profile'
 
@@ -50,6 +51,7 @@ export function RoutesApp() {
       <Route path="/TextInputTest" element={<InputTextTest />} />
       <Route path="/Footer" element={<FooterTest />} />
       <Route path="/ContactBlock" element={<ContactBlockTest />} />
+      <Route path="/SelectOptionTest" element={<SelectOptionTest />} />
     </Routes>
   )
 }
