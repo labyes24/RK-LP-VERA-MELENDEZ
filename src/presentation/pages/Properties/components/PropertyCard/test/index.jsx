a800acd0288c54aeb256d97bf1fc44d1540563c3
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-11-02 10:50:48 -0300

    feat/TASK-0021/SUB-0002: Added environment test component

---
+++ src/presentation/pages/Properties/components/PropertyCard/test/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './styles'
+import { PropertyCard } from '../'
+export function PropertyCardTest() {
+  return (
+    <Container>
+      <PropertyCard />
+    </Container>
+  )
+}
