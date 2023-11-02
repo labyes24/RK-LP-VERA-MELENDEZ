36c94d104d31ddea9e1e9477a8ca0fa01a4294ad
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-11-02 11:05:51 -0300

    style/TASK-0021/SUB-0002: Stylized PropertyCard container

--- src/presentation/pages/Properties/components/PropertyCard/styles.js
+++ src/presentation/pages/Properties/components/PropertyCard/styles.js
@@ -1,3 +1,19 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: grid;
+  padding: 3.2rem 1.6rem;
+  gap: 1.6rem;
+  border-radius: 0.8rem;
+  width: 36rem;
+  background-color: ${({ theme }) => theme.COLORS['blue-titan']};
+
+  @media (min-width: 1024px) {
+    display: flex;
+    gap: 4.8rem;
+    padding: 4.8rem;
+    border-radius: 1.6rem;
+
+    width: 105.8rem;
+  }
+`
