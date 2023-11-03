d717cb9ff639ed87542a47a27e8e1f3f875674d5
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-11-03 11:12:19 -0300

    refactor/FEAT-0003/TASK-0002: removed comments

--- src/presentation/pages/Properties/components/Modal/test/index.jsx
+++ src/presentation/pages/Properties/components/Modal/test/index.jsx
@@ -4,22 +4,18 @@ import { Modal } from '../index'
 import { Container } from './styles'
 
 export function ModalTest() {
-  // state que define se o modal está aberto ou fechado, utilizado na prop isOpen (linha 18)
   const [isModalOpen, setIsModalOpen] = useState(false)
 
-  // função para abrir o modal, utilizada na prop openFn (linha 25)
   function openModal() {
     setIsModalOpen(true)
   }
 
-  // função para fechar o modal, utilizada na prop closeFn (linha 25)
   function closeModal() {
     setIsModalOpen(false)
   }
 
   return (
     <Container>
-      {/* botão para abrir o modal */}
       <button onClick={openModal}>Open Modal</button>
 
       <Modal isOpen={isModalOpen} closeFn={closeModal} />
