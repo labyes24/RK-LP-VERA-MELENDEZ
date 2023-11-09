db5f057fcfc6857025a23dad887ca7d8e2aaac15
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-11-09 14:08:43 -0300

    fix/FIX-0005/TASK-0012: updated ContactBlock font sizes

--- src/presentation/components/ContactBlock/styles.js
+++ src/presentation/components/ContactBlock/styles.js
@@ -21,7 +21,7 @@ export const Container = styled.aside`
 
     color: ${({ theme }) => theme.COLORS.white};
     font-family: 'Roboto', sans-serif;
-    font-size: 20px;
+    font-size: 2rem;
     font-style: normal;
     font-weight: 400;
     line-height: 140%;
@@ -32,7 +32,7 @@ export const Container = styled.aside`
       column-gap: 1.6rem;
 
       p {
-        word-break: break-all;
+        word-break: break-word;
 
         a {
           text-decoration: none;
@@ -58,12 +58,25 @@ export const Container = styled.aside`
       align-items: start;
     }
 
-    @media (max-width: 768px) {
-      font-size: 1.4rem;
+    :nth-child(3) {
+      font-size: 2.4rem;
 
-      :nth-child(2),
-      li:nth-child(3) {
-        font-size: 1.6rem;
+      img {
+        width: 3.6rem;
+        height: 3.6rem;
+      }
+    }
+
+    @media (max-width: 768px) {
+      font-size: 1.6rem;
+
+      :nth-child(3) {
+        font-size: 2rem;
+
+        img {
+          width: 3rem;
+          height: 3rem;
+        }
       }
     }
   }
