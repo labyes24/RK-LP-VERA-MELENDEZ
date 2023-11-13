cf9a58848468ac33fc0bc7752542bbccb3144ed0
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-11-13 11:08:59 -0300

    refactor/TASK-0029: refactoring BrokerImage

--- src/presentation/components/BrokerImage/index.jsx
+++ src/presentation/components/BrokerImage/index.jsx
@@ -1,12 +1,13 @@
 import { Container } from './styles'
-import CorretorImg from '../../assets/CorretorImg.png'
 import Elipses from '../../assets/elipses.svg'
+import { useBrokerProfile } from '../../../data/BrokerData'
 
 export function BrokerImage({ removeelipses = false }) {
+  const brokerData = useBrokerProfile()
   return (
     <Container $removeelipses={removeelipses}>
       <div className="Images">
-        <img src={CorretorImg} alt="Imagem do Corretor" />
+        <img src={brokerData.profileImage} alt="Imagem do Corretor" />
         {!removeelipses && <img src={Elipses} alt="" />}
       </div>
     </Container>
