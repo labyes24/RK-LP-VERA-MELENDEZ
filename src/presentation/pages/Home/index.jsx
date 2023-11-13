96fa284c269c28b67f393f04669c5c8c230c2dad
henrico <henrico.an@gmail.com>
2023-11-13 14:20:48 -0300

    refactor/TASK-0031: update homepage infos from BrokerData.js

--- src/presentation/pages/Home/index.jsx
+++ src/presentation/pages/Home/index.jsx
@@ -3,33 +3,12 @@ import { Video } from '../../components/Video'
 import { Card } from '../../components/Card'
 import { WhatsappButton } from '../../components/WhatsappButton'
 
+import { useBrokerProfile } from '../../../data/BrokerData'
+
 export function Home() {
-  const quotes = [
-    {
-      id: crypto.randomUUID(),
-      quote:
-        'Life is like riding a bicycle. To keep your balance, you must keep moving.',
-      author: 'Albert Einstein',
-    },
-    {
-      id: crypto.randomUUID(),
-      quote:
-        'Winnetou apresentou-se bastante solícito e acolhedor. Acompanhou cada etapa do processo com bastante cuidado. Profissional excelente, capacitado e atencioso com cada detalhe.',
-      author: 'Paola Oliveira,  Atriz',
-    },
-    {
-      id: crypto.randomUUID(),
-      quote:
-        'Fazer negócios com a RK Imóveis é garantia de sucesso. Eu já comprei, vendi, fui locador e locatário de imóveis, e em todas as negociações fui bem atendido.',
-      author: 'Manoel de Barros, Poeta',
-    },
-    {
-      id: crypto.randomUUID(),
-      quote:
-        'Excelente atendimento, quando anunciei meu imóvel, foram muito atenciosos, e venderam ele muito rápido. Empresa séria e transparente. Recomendo!',
-      author: 'Madam CJ Walker, empresária',
-    },
-  ]
+  const broker = useBrokerProfile()
+  const quotes = broker.comments
+  const brokerPhoneNumber = broker.phone
 
   return (
     <Container>
@@ -43,7 +22,7 @@ export function Home() {
           ))}
         </div>
       </Content>
-      <WhatsappButton />
+      <WhatsappButton phoneNumber={brokerPhoneNumber} />
     </Container>
   )
 }
