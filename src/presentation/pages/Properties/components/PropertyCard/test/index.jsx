84c76ad30dca2f2402c4dcdb946b75d4d9db9ccf
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-11-13 11:19:32 -0300

    feat/TASK-0021/SUB-0002: Added cardProperty test

--- src/presentation/pages/Properties/components/PropertyCard/test/index.jsx
+++ src/presentation/pages/Properties/components/PropertyCard/test/index.jsx
@@ -1,9 +1,11 @@
 import { Container } from './styles'
 import { PropertyCard } from '../'
+import { useProperties } from '../../../../../../data/RealStateData'
 export function PropertyCardTest() {
+  const properties = useProperties()
   return (
     <Container>
-      <PropertyCard />
+      <PropertyCard propertyInfo={properties[0]} />
     </Container>
   )
 }
