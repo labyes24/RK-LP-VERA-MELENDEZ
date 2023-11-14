6b69e542c961cc8f9a22325e8b16131cb358c40b
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-11-14 08:53:24 -0300

    feat/TASK-0039: Added modal component into properties page

--- src/presentation/pages/Properties/index.jsx
+++ src/presentation/pages/Properties/index.jsx
@@ -4,21 +4,40 @@ import { Carousel } from '../../components/Carousel'
 import { SplideSlide } from '@splidejs/react-splide'
 import { PropertyCard } from './components/PropertyCard'
 import { WhatsappButton } from '../../components/WhatsappButton'
+import { Modal } from './components/Modal'
+
 import { useBrokerProfile } from '../../../data/BrokerData'
+import { useState } from 'react'
 
 export function Properties() {
   const properties = useProperties()
   const { phone } = useBrokerProfile()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [PropertyCodeInfo, setPropertyCodeInfo] = useState('')
+
+  function handleGetPropertyCodeInfo(code) {
+    setIsModalOpen(true)
+    setPropertyCodeInfo(code)
+  }
+
   return (
     <Container>
       <h1>Encontre o imóvel perfeito para você e sua família!</h1>
       <Carousel option="cardProperty" length={properties.length}>
         {properties.map(property => (
           <SplideSlide key={property.id}>
-            <PropertyCard propertyInfo={property} />
+            <PropertyCard
+              propertyInfo={property}
+              onInterestingProperty={handleGetPropertyCodeInfo}
+            />
           </SplideSlide>
         ))}
       </Carousel>
+      <Modal
+        isOpen={isModalOpen}
+        onCloseModal={() => setIsModalOpen(false)}
+        propertyCode={PropertyCodeInfo}
+      />
       <WhatsappButton phoneNumber={phone} />
     </Container>
   )
