d49bf60726634c26a40a926242e0624c5ea1582d
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-11-22 16:08:16 -0300

    style/TASK-0018: styling the PrivatePolicy page

--- src/presentation/pages/PrivatePolicy/index.jsx
+++ src/presentation/pages/PrivatePolicy/index.jsx
@@ -16,7 +16,7 @@ export function PrivatePolicy() {
             <span>
               RK Imóveis Rua das Gaivotas, 1709 – Salas 102 e 104 – Ingleses do
               Rio Vermelho | 88058-500 | Florianópolis | SC Telefone: (48)
-              3266-0866 E-mail: contato@rkimoveis.com.br
+              3266-0866 E-mail:contato@rkimoveis.com.br
             </span>
             Esta declaração de privacidade de dados tem como objetivo divulgar
             informações sobre a coleta, o processamento e o uso (doravante
@@ -25,14 +25,14 @@ export function PrivatePolicy() {
           <strong>2. Coleta e uso de dados pessoais</strong>
           <p>
             Dados pessoais são informações que permitem identificação, tais como
-            o nome, e-mail, telefone ou endereço. A RK Imóveis não coleta dados
-            pessoais, exceto quando especificamente fornecidos (p. ex., no
+            o nome, e-mail, telefone ou endereço. <br />A RK Imóveis não coleta
+            dados pessoais, exceto quando especificamente fornecidos (p. ex., no
             cadastro para recebimento de newsletters, solicitação de orçamento
             ou solicitações de contato comercial) e salvo consentimento para a
-            sua utilização. Armazenamos, usamos ou transferimos seus dados
-            pessoais apenas com seu consentimento e em situações específicas
-            como, por exemplo, para responder às suas perguntas, pedidos de
-            informações ou para informá-lo sobre novidades.
+            sua utilização. <br /> Armazenamos, usamos ou transferimos seus
+            dados pessoais apenas com seu consentimento e em situações
+            específicas como, por exemplo, para responder às suas perguntas,
+            pedidos de informações ou para informá-lo sobre novidades.
           </p>
         </section>
       </main>
--- src/presentation/pages/PrivatePolicy/styles.js
+++ src/presentation/pages/PrivatePolicy/styles.js
@@ -1,5 +1,74 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Container = styled.div`
+  padding: 3.2rem 2.5rem;
 
+  > main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+      font-size: 2.4rem;
+      font-family: ${({ theme }) => theme.FONTS.Primary};
+      text-align: center;
+
+      margin-bottom: 4rem;
+    }
+
+    section {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+
+      > strong {
+        font-family: ${({ theme }) => theme.FONTS.Secondary};
+        font-weight: 800;
+        margin-bottom: 2rem;
+        font-size: 1.8rem;
+      }
+
+      > p {
+        display: flex;
+        flex-direction: column;
+        font-size: 1.6rem;
+        font-family: ${({ theme }) => theme.FONTS.Primary};
+        line-height: 2rem;
+        text-align: justify;
+        margin-bottom: 2rem;
+
+        span {
+          width: 22rem;
+          margin: 2rem 0;
+          font-weight: 500;
+          line-height: 2rem;
+          color: ${({ theme }) => theme.COLORS['zinc-100']};
+        }
+      }
+
+      @media (min-width: 1024px) {
+        > strong {
+          font-size: 2.4rem;
+        }
+
+        > p {
+          font-size: 1.8rem;
+          line-height: 2.5rem;
+        }
+
+        > span {
+          width: 80rem;
+        }
+      }
+    }
+    @media (min-width: 1024px) {
+      padding: 4.2rem 3.5rem;
+      > h1 {
+        align-self: center;
+        font-size: 3.6rem;
+        line-height: 100%;
+      }
+    }
+  }
 `
