b4eb619ac471af6b64ac61af0de191b0de56f483
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-11-29 13:24:08 -0300

    styles/FEAT-0010: added scrollbar styles

--- src/presentation/styles/global.js
+++ src/presentation/styles/global.js
@@ -18,8 +18,25 @@ export const GlobalStyle = createGlobalStyle`
     color: ${({ theme }) => theme.COLORS.white};
     font-family: ${props => props.theme.FONTS.Primary};
     font-size: 1.6rem;
-    min-height: 99vh;
+    min-height: 99vh;  
+  }
 
+  body::-webkit-scrollbar {
+    width: 8px;
+  }
+  body::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.COLORS['zinc-150']};
+    border-radius: 10px;
+    }
+  body::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.COLORS['blue-sky-50']};
+    border-radius: 10px;
+  }
+  body::-webkit-scrollbar-thumb:hover {
+    background-color: ${({ theme }) => theme.COLORS['blue-sky-100']};    
+  }
+  body::-webkit-scrollbar-thumb:active {
+    background-color: ${({ theme }) => theme.COLORS['blue-sky-50']};
   }
 
   #root {
