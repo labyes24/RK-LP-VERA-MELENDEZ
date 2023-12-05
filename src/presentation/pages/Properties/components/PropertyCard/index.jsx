0c6cc39223c15aaa29524f0946bdb665b9a1c2e7
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-12-05 12:54:48 -0300

    style/RK-0001: change garage svg on the Tag component

--- src/presentation/pages/Properties/components/PropertyCard/index.jsx
+++ src/presentation/pages/Properties/components/PropertyCard/index.jsx
@@ -11,7 +11,7 @@ import {
 import { Tag } from '../../../../components/Tag'
 import { Carousel } from '../../../../components/Carousel'
 
-import garageIcon from '../../../../assets/garage-icon.svg'
+import garage_icon2 from '../../../../assets/garage_icon2.svg'
 import roomIcon from '../../../../assets/room-icon.svg'
 import squareIcon from '../../../../assets/square-icon.svg'
 import showerIcon from '../../../../assets/shower-icon.svg'
@@ -20,7 +20,7 @@ import { SplideSlide } from '@splidejs/react-splide'
 import { useState } from 'react'
 
 const IconsMap = {
-  garage: garageIcon,
+  garage: garage_icon2,
   rooms: roomIcon,
   square: squareIcon,
   bathrooms: showerIcon,
