25c357cf912bddbe3ae07f57140330c29b205f00
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2024-02-23 16:15:44 -0300

    feat/RLT-8: Displays video according to device screen orientation

--- src/presentation/pages/Home/index.jsx
+++ src/presentation/pages/Home/index.jsx
@@ -7,17 +7,30 @@ import { Carousel } from '../../components/Carousel'
 
 import { useBrokerProfile } from '../../../data/BrokerData'
 
+import videoMobile from '../../assets/rk-video-mobile.mp4'
+import videoDesktop from '../../assets/rk-video-desktop.mp4'
+
 export function Home() {
   const broker = useBrokerProfile()
   const { comments: quotes } = useBrokerProfile()
   const brokerPhoneNumber = broker.phone
+
+  const isDeviceScreenPortrait = window.matchMedia(
+    '(orientation: portrait)',
+  ).matches
 
   return (
     <Container>
       <div className="bg-img" />
       <Content>
         <h2>Oportunidades únicas não esperam por ninguém!</h2>
-        <Video />
+
+        {isDeviceScreenPortrait ? (
+          <Video src={videoMobile} />
+        ) : (
+          <Video src={videoDesktop} />
+        )}
+
         <Carousel option="card" length={quotes.length}>
           {quotes.map(quote => (
             <SplideSlide key={quote.id}>
