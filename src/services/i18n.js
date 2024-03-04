6f8a93a5414de3136a0aa712214b4abf4ca64cd0
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2024-03-04 16:13:57 -0300

    chore: config i18n structure

---
+++ src/services/i18n.js
@@ -0,0 +1,26 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import ptTranslations from './locales/pt.json'
+import esTranslations from './locales/es.json'
+
+const resources = {
+  pt: {
+    translation: { ...ptTranslations },
+  },
+  es: {
+    translation: { ...esTranslations },
+  },
+}
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: 'pt',
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  })
+
+export default i18n
