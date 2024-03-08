3f5ef89ee8d711b6c1caa0f3f34faecf3efb6fd1
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-03-08 07:10:58 -0300

    [RLT-42] [Politica de Privcidade] Internacionalização da página Política de Privacidade

--- src/presentation/pages/PrivatePolicy/index.jsx
+++ src/presentation/pages/PrivatePolicy/index.jsx
@@ -2,40 +2,35 @@ import { Container } from './styles'
 import { WhatsappButton } from '../../components/WhatsappButton'
 import { useBrokerProfile } from '../../../data/BrokerData'
 
+import { useTranslation } from 'react-i18next'
+
 export function PrivatePolicy() {
   const { phone } = useBrokerProfile()
+  const { t } = useTranslation()
+
   return (
     <Container>
       <main>
-        <h1>Política de Privacidade</h1>
+        <h1>{t('privatePolicy.title')}</h1>
         <section>
-          <strong>1. Instituição responsável</strong>
+          <strong>{t('privatePolicy.caption')}</strong>
           <p>
-            A empresa responsável por coletar, processar e usar dados pessoais
-            nos termos da Lei Geral de Proteção de Dados Pessoais (LGPDP) é:
+            {t('privatePolicy.paragraph-one')}
             <span>
               <strong className="title">RK Imóveis</strong> <br />
               Rua das Gaivotas, 1709 – Sala 104 <br />
               Ingleses do Rio Vermelho - Florianópolis-SC <br />
               CEP: 88058-500 <br />
-              Telefone: (48) 3266-0866 <br />
-              E-mail: contato@rkimoveis.com.br
+              {t('privatePolicy.phone')}: (48) 3266-0866 <br />
+              {t('privatePolicy.email')}: contato@rkimoveis.com.br
             </span>
-            Esta declaração de privacidade de dados tem como objetivo divulgar
-            informações sobre a coleta, o processamento e o uso (doravante
-            denominado “uso”) de dados pessoais.
+            {t('privatePolicy.paragraph-two')}
           </p>
-          <strong>2. Coleta e uso de dados pessoais</strong>
+          <strong>{t('privatePolicy.caption-two')}</strong>
           <p>
-            Dados pessoais são informações que permitem identificação, tais como
-            o nome, e-mail, telefone ou endereço. <br />A RK Imóveis não coleta
-            dados pessoais, exceto quando especificamente fornecidos (p. ex., no
-            cadastro para recebimento de newsletters, solicitação de orçamento
-            ou solicitações de contato comercial) e salvo consentimento para a
-            sua utilização. <br /> Armazenamos, usamos ou transferimos seus
-            dados pessoais apenas com seu consentimento e em situações
-            específicas como, por exemplo, para responder às suas perguntas,
-            pedidos de informações ou para informá-lo sobre novidades.
+            {t('privatePolicy.text-one')} <br />
+            {t('privatePolicy.text-two')} <br />
+            {t('privatePolicy.text-three')}
           </p>
         </section>
       </main>
