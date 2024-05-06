7eea2a3975f17225776f85345b4c27126c21878e
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-05-06 14:04:07 -0300

    style/RLT-56: update Input style

--- src/presentation/components/PhoneInput/styles.js
+++ src/presentation/components/PhoneInput/styles.js
@@ -5,21 +5,18 @@ import 'react-international-phone/style.css'
 
 export const StyledPhoneInput = styled(PhoneInput)`
   border: 1px solid
-    ${({ theme, $isValid }) =>
-      $isValid ? theme.COLORS['blue-sky-600'] : theme.COLORS.tomate};
+    ${({ theme, isValid }) =>
+      isValid ? theme.COLORS['blue-sky-600'] : theme.COLORS.tomate};
   color: ${({ theme }) => theme.COLORS['blue-sky-700']};
 
   border-radius: 5px;
 
-  font-family: ${({ theme }) => theme.FONTS.Primary};
-  font-size: 1.4rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 100%;
-
   .phone-number-input {
     width: 100%;
     background-color: ${({ theme }) => theme.COLORS['zinc-50']};
+    font-family: ${({ theme }) => theme.FONTS.Primary};
+    line-height: 100%;
+    font-size: 1.4rem;
   }
 
   &::placeholder {
