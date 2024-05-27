9c7358f446d09328c87203debef8be429888b53c
Evandro <dam450@duck.com>
2024-05-27 19:07:31 -0300

    chore/RLT-98: config browser-languagedetector to i18n

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -1,23 +1,28 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 import ptTranslations from './locales/pt.json'
 import esTranslations from './locales/es.json'
 
 const resources = {
   pt: {
-    translation: { ...ptTranslations },
+    common: { ...ptTranslations },
   },
   es: {
-    translation: { ...esTranslations },
+    common: { ...esTranslations },
   },
 }
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+    ns: ['common'],
+    defaultNS: 'common',
+    supportedLngs: ['es', 'pt'],
     resources,
-    lng: 'pt',
+    fallbackLng: 'pt',
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
