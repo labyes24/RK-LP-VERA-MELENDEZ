f75ee2ef2fccdf25901da0026fc47a9262ca6219
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-06-17 20:33:50 -0300

    hotfix/RLT-110: change countries and improve validation

--- src/presentation/lib/reactInternationalPhone.js
+++ src/presentation/lib/reactInternationalPhone.js
@@ -1,14 +1,26 @@
-import { defaultCountries } from 'react-international-phone'
+import { defaultCountries, parseCountry } from 'react-international-phone'
 
-export const countries = defaultCountries.map(country => {
-  const countrySlug = country[1]
+export const countries = defaultCountries
+  .filter(country => {
+    const { iso2 } = parseCountry(country)
+    return ['br', 'ar', 'py', 'uy'].includes(iso2)
+  })
+  .map(country => {
+    const countrySlug = country[1]
 
-  if (countrySlug === 'br') {
-    const newCountry = [...country]
-    newCountry[3] = '(..) .....-....' // new Brazil mask
+    if (countrySlug === 'br') {
+      const newCountry = [...country]
+      newCountry[3] = '(..) .....-....' // new Brazil mask
 
-    return newCountry
-  }
+      return newCountry
+    }
 
-  return country
-})
+    if (countrySlug === 'ar') {
+      const newCountry = [...country]
+      newCountry[3] = '. ... ... ....' // new Arg mask
+
+      return newCountry
+    }
+
+    return country
+  })
